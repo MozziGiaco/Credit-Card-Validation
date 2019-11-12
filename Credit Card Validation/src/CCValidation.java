ccb045b0ce501b28983709daca9c39cf6d1cb880
nicholas.giacomozzi <nicholas.giacomozzi@mu-c1-03.mullen.pvt.k12.co.us>
2019-11-12 11:57:09 -0700

    Commit

---
+++ Credit Card Validation/src/CCValidation.java
@@ -0,0 +1,20 @@
+
+public class CCValidation
+	{
+		static long validCard = (long) 5424180123456789.;
+		static long invalidCard = (long) 5424180123456788.;
+
+		public static void main(String[] args)
+			{
+				doubleNumbers();
+			}
+
+		public static void doubleNumbers()
+			{
+				for (int i = 0; i < invalidCard; i = i + 2)
+					{
+
+					}
+			}
+
+	}
